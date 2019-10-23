b6862d118947a004ffcf6a4d6ddc0bb0b7ca2dd3
axell-brendow <breno.axel@gmail.com>
2019-10-22 21:16:13 -0300

    Começa a tirar a recursividade do Quicksort

--- src/algoritmos/Quicksort.java
+++ src/algoritmos/Quicksort.java
@@ -1,5 +1,7 @@
 package algoritmos;
 
+import java.util.ArrayList;
+
 /**
  * Classe que implementada o algoritmo de ordenação seleção
  * Implementação para inteiros
@@ -8,6 +10,27 @@ package algoritmos;
 
 
 public class Quicksort  implements AlgoritmoDeOrdenacao{
+	
+	class Status<DADO extends Comparable<DADO>>
+	{
+		public int i, j, esq, dir;
+		/**
+		 * Essa variável
+		 */
+		int particoes;
+		
+		public Status(int i, int j, int esq, int dir, int particoes)
+		{
+			this.i = i;
+			this.j = j;
+			this.esq = esq;
+			this.dir = dir;
+			this.particoes = particoes;
+		}
+		
+		public Status(int esq, int dir) { this(-1, -1, esq, dir, 0); }
+	}
+	
     /**
      * Algoritmo de ordenacao Quicksort.
      */
@@ -21,8 +44,51 @@ public class Quicksort  implements AlgoritmoDeOrdenacao{
      * @param dir fim do array a ser ordenado
      */
     private <DADO extends Comparable<DADO>> DADO[] quicksort(int esq, int dir, DADO[] array) {
-        int i = esq, j = dir;
-        DADO pivo = array[(dir+esq)/2]; //pega a posição do pivo
+        
+    	ArrayList<Status<DADO>> stack = new ArrayList<>();
+    	// Trocar a variável status é equivalente a uma chamada recursiva pois
+    	// o quicksort sempre estará particionando a partição do status
+    	Status<DADO> status = new Status<>(esq, dir);
+    	// Entrada problemática: 2 3 3 1 3
+    	do
+    	{
+        	quicksort(status, array); // Particiona de esq a dir com base no pivo
+        	// Adiciona as variáveis da partição na pilha caso essa
+        	// participação não tenha filhas à esquerda nem à direita
+        	if (status.particoes == 0) stack.add(status);
+        	
+            if (status.particoes == 0 && status.esq < status.j) {
+            	status = new Status<>(status.esq, status.j);
+            	continue;
+            }
+            
+            status.particoes++;
+            
+            if (status.particoes == 1 && status.i < status.dir) {
+            	status = new Status<>(status.i, status.dir);
+            	continue;
+            }
+            
+            status.particoes++;
+            stack.remove(stack.size() - 1); // Remove o último status da pilha
+            
+            // Obtém o status no topo
+            if (!stack.isEmpty()) status = stack.get(stack.size() - 1);
+            
+    	} while (!stack.isEmpty());
+
+        return array;
+    }
+
+    /**
+     * Faz o particionamento da parte indica no status.
+     * 
+     * @param status Objeto com o estado da partição atual.
+     * @param array Arranjo a ser ordenado.
+     */
+    private <DADO extends Comparable<DADO>> void quicksort(Status<DADO> status, DADO[] array) {
+        int i = status.esq, j = status.dir;
+        DADO pivo = array[(status.dir + status.esq)/2]; //pega a posição do pivo
 
         while (i <= j) {
             while (array[i].compareTo(pivo) < 0){
@@ -39,14 +105,9 @@ public class Quicksort  implements AlgoritmoDeOrdenacao{
                 j--;
             }
         }
-        if (esq < j) {
-            array = quicksort(esq, j, array);
-        }
-        if (i < dir) {
-            array = quicksort(i, dir, array);
-        }
-
-        return array;
+        
+        status.i = i;
+        status.j = j;
     }
 
     public <DADO extends Comparable<DADO>> void swap(int i, int j, DADO[] array) {
